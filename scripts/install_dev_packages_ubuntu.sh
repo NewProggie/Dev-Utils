ef2f8286054e925bfbf4cddde9df1389822fed6b
Kai Wolf <kai.wolf@gmail.com>
2016-02-05 09:50:33 +0100

    Fix bash script error semicolons

--- scripts/install_dev_packages_ubuntu.sh
+++ scripts/install_dev_packages_ubuntu.sh
@@ -10,13 +10,13 @@ sed -i 's/# deb http/deb http/g' /etc/apt/sources.list
 sed -i 's/# deb-src http/deb-src http/g' /etc/apt/sources.list
 
 read -p "==> Enable CMake 3.X ppa? " -n 1 -r; echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
+if [[ $REPLY =~ ^[Yy]$ ]] ; then
     sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
 fi
 
 use_c11=false
 read -p "==> Enable C/C++11 ppa? " -n 1 -r; echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
+if [[ $REPLY =~ ^[Yy]$ ]] ; then
     SW_PACKS+=("gcc-5" "g++-5")
     use_c11=true
     sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
@@ -31,7 +31,7 @@ sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.6 90
 sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.6 90
 sudo update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-3.6 90
 
-if [ "${use_c11}" = true ]; then
+if [ "${use_c11}" = true ] ; then
     sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 90
     sudo update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-4.9 90
 fi
