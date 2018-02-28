f16f843126fdde529cedb42622c5e093791169c8
Kai Wolf <kai.wolf@gmail.com>
2018-02-28 15:13:38 +0100

    Check for color support
    Change-Id: I3806ae648e5424e3931d0545f412c6a29447067f

--- scripts/slog.py
+++ scripts/slog.py
@@ -2,13 +2,36 @@
 
 import os
 from subprocess import getoutput
-from sys import argv
+from sys import argv, stdout, platform
 
 newline = '\r\n' if os.name == 'nt' else '\n'
 
 W = '\033[0m'  # white (normal)
 R = '\033[31m'  # red
 B = '\033[36m'  # blue
+
+
+def term_supports_color():
+    supported_platform = platform != 'Pocket PC' and (platform != 'win32' or
+                                                      'ANSICON' in os.environ)
+    is_a_tty = hasattr(stdout, 'isatty') and stdout.isatty()
+    return supported_platform and is_a_tty
+
+
+def print_svn_info_colored(rev, user, date, message):
+    print("*%s changeset:   %s%s" % (R, rev, W))
+    print("%s|%s user:        %s" % (B, W, user))
+    print("%s|%s date:        %s" % (B, W, date))
+    print("%s|%s summary:     %s" % (B, W, message.strip()))
+    print("%s|%s" % (B, W))
+
+
+def print_svn_info(rev, user, date, message):
+    print("* changeset:   %s" % rev)
+    print("| user:        %s" % user)
+    print("| date:        %s" % date)
+    print("| summary:     %s" % message.strip())
+    print("|")
 
 
 def main(args):
@@ -19,11 +42,10 @@ def main(args):
         if len(parts) < 2: continue
         info, message = parts
         rev, user, date, _ = list(map(str.strip, info.split('|')))
-        print("*%s changeset:   %s%s" % (R, rev, W))
-        print("%s|%s user:        %s" % (B, W, user))
-        print("%s|%s date:        %s" % (B, W, date))
-        print("%s|%s summary:     %s" % (B, W, message.strip()))
-        print("%s|%s" % (B, W))
+        if term_supports_color():
+            print_svn_info_colored(rev, user, date, message.strip())
+        else:
+            print_svn_info(rev, user, date, message.strip())
 
 
 if __name__ == "__main__":
